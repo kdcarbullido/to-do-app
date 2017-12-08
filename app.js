00621623ee2a55a11b6d5c13f4edaa6abef7e4e8
kdcarbullido <kdcarbullido@gmail.com>
2017-12-08 11:29:06 -0600

    Added mod2assignment8 bonus work, to store todos

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ function onReady() {
 
     let id = 0;
     let toDos = [];
+    var strToDos = localStorage.getItem("storedToDos");
+    if (strToDos !== null) {
+        toDos = JSON.parse(strToDos);
+        id = toDos.length;
+    }
+
+
     const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
@@ -17,32 +24,38 @@ function onReady() {
             id: id
         });
         id++;
-       // newToDoText.value = '';  Should this be here. shows in chkpoint illustration, but not instructed.  This statement also appears in addtoDo.eventlistener function.
-
+        saveToDos();    // save todos to storage
         renderTheUI();
     }
 
     function deleteToDo (id) {
         toDos = toDos.filter(item => item.id !== id);
+        saveToDos();
     }
 
+    function saveToDos () {
+        strToDos = JSON.stringify(toDos);
+        localStorage.setItem("storedToDos", strToDos);
+    }
 
 
     function renderTheUI() {
         const toDoList = document.getElementById('toDoList');
         toDoList.textContent = '';
-        toDos.forEach(function(toDos){
+        toDos.forEach(function(toDo){
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
             checkbox.type = "checkbox";
             const title = document.createElement('span');
             const completeText = document.createTextNode('Check if To-Do is completed . . .  ');
-            title.textContent = toDos.title;
+            title.textContent = toDo.title;
+            if (toDo.complete) {
+                checkbox.checked = true;
+            }
 
             const delBtn = document.createElement('button');
             const delBtnText = document.createTextNode('Click to Delete this To-Do');
             delBtn.appendChild(delBtnText);
-
 
             toDoList.appendChild(newLi);
             newLi.appendChild(title);
@@ -50,9 +63,17 @@ function onReady() {
             newLi.appendChild(completeText);
             newLi.appendChild(delBtn);
             delBtn.addEventListener('click', event => {
-                deleteToDo(toDos.id);
+                deleteToDo(toDo.id);
                 renderTheUI();
             });
+
+            checkbox.addEventListener('click', event => {
+                toDo.complete = !toDo.complete;
+                saveToDos();
+            });
+
+
+
         });
     }
 
